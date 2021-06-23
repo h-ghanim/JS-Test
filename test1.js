7831ba6267895ae5d64562f25c72b696e786fe1e
Heba Ghanim <hghanim@asaltech.com>
2021-06-23 09:59:29 +0300

    added test2 file

--- test1.js
+++ test1.js
@@ -1,9 +1,21 @@
 const person = {
     firstName: "Heba",
     lastName: "Ghanim",
-    getFullName:function (){
+    getFullName: function(){
+        console.log(this);
         return this.firstName + " " + this.lastName;
     }
 }
 
-console.log(person.getFullName());
+
+function sum(x, y){
+    console.log(this)
+    return x + y;
+}
+//console.log(sum(3, 5));
+
+
+//console.log(person.getFullName());
+this.b = 10;
+
+console.log(this == window);
